4d802eff80d19c8cd8b890ae74ac6017e4562b07
Ziad Mostafa <mohammedziad.badr@gmail.com>
2021-10-31 20:22:03 +0100

    implement kdtree insert function
    Signed-off-by: Ziad Mostafa <mohammedziad.badr@gmail.com>

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -40,7 +40,29 @@ struct KdTree
 	{
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root 
+		// if(this->root == nullptr){
+		// 	this->root = new Node(point,id);
+		// 	return;
+		// }
+		_insert(&this->root,point,id,0);
 
+	}
+
+	void _insert(Node** root, std::vector<float>& point,int& id, int depth){
+		
+		if(*root == nullptr){
+			*root = new Node(point,id);
+		}else{
+			
+			unsigned int current_depth = depth % 2 ; // two planes x and y;
+			if(point[current_depth] < (*root)->point[current_depth]){
+				_insert(&((*root)->left), point, id, depth + 1);
+			}else{
+				_insert(&((*root)->right), point, id, depth + 1);
+			}
+		}
+
+		// return *root;
 	}
 
 	// return a list of point ids in the tree that are within distance of target
